aedfa9a8f55b795cc999435d897ef8e7218a8abf
Alp Tarla <alptarla@gmail.com>
2022-02-06 17:11:12 +0300

    Add header to layout

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,5 +1,34 @@
+import Image from 'next/image'
+import { useState } from 'react'
+import { Moon, Plus } from 'react-feather'
+
 function Header() {
-  return <div className="p-5">Header</div>
+  const [shouldShowMain, setShouldShowMain] = useState(false)
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false)
+
+  const handleAdd = () => {
+    setShouldShowMain(true)
+    setShowQuickAddTask(true)
+  }
+
+  return (
+    <div className="bg-red-500 text-white">
+      <div className="container mx-auto py-5">
+        <nav className="flex items-center gap-5 justify-between">
+          <Image src="/images/logo.png" alt="Todoist" width="30" height="30" />
+          <div>
+            <button type="button" onClick={handleAdd} className="mr-5">
+              <Plus />
+            </button>
+            <button type="button" onClick={() => {}} title="Toggle app theme">
+              {/* <Sun /> */}
+              <Moon />
+            </button>
+          </div>
+        </nav>
+      </div>
+    </div>
+  )
 }
 
 export default Header
--- components/Layout/Sidebar/index.tsx
+++
@@ -1,5 +0,0 @@
-function Sidebar() {
-  return <div className="h-full w-full p-5">Sidebar</div>
-}
-
-export default Sidebar
